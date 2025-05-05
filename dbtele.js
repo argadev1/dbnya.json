046b56fe3436279c455b0146e45d8e0e2e8f7d54
argadev1 <argabtm777@gmail.com>
2025-05-06 02:29:00 +0700

    dbtele.js

--- dbtele.js
+++ dbtele.js
@@ -1,6 +1,6 @@
 {
   "tokens": [
     "7937847165:AAG51GsFAyMMaHS8LXLPhhmTRP6Lqb-VO-0",
-    "7726684474:AAHvn_kVE0I1UZbVm4S8-wb9kYOkW5m8AAk"
+    "7538107337:AAHc0s4qn-oRhof4pNGw5IK8UfZ8vQ5ojiI"
   ]
 }
